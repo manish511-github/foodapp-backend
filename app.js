e8a096df312c35099c8241b3ca8da4a6d1c7cc6f
Manish Singh <manishsingh@Manishs-MacBook-Air.local>
2022-07-05 01:46:03 +0530

    first update2f

--- app.js
+++ app.js
@@ -1,119 +1,30 @@
-const express=require("express");
-const { get } = require("lodash");
+const express = require("express");
 const app = express();
-var cors=require('cors');
-app.use(cors());
-
+var cors = require('cors');
+app.use(cors()) ;
 app.use(express.static('public/build'));
 
-
-const cookieParser=require('cookie-parser'); 
+const cookieParser=require('cookie-parser');
+//middleware func-> post, front-> json
 app.use(express.json()); //global middleware 
-
-const PORT=process.env.PORT||'8080'
-app.set("port",PORT);
-
-
-
+const port=process.env.PORT || 5000;
+app.listen(port,function(){
+    console.log(`server listening on port ${port}`); 
+});
 app.use(cookieParser());
 
-// let users=[
-//     {
-//         'id':1,
-//         'name':"dsm f"
-//     },
-//     {
-//         'id':2,
-//         'name':"dsmfk"
 
-//     },
-//     {
-//         'id':3,
-//         'name':"dnfndkf"
-//     }
-// ];
-
-const userRouter=require('./Routers/userRouter');
-const planRouter=require('./Routers/planRouter');
-const reviewRouter=require('./Routers/reviewRouter');
-const bookingRouter=require('./Routers/bookingRouter')
-
-app.use("/user",userRouter);
-
-app.use("/plans",planRouter);
-
-app.use("/review",reviewRouter);
-
-app.use("/booking",bookingRouter);
-
-
-
-// app.use("/auth",authRouter);
-const planModel=require('./models/planModel');
-
-
-
-
-
-
-
-
-// app.get('/user',(req,res)=>{
-//     console.log(req.query);
-
-//     res.send(users);
-
-// })
-// app.post('/user',(req,res)=>{
-//     console.log(req.body);
-//     users=req.body;
-
-//     res.json({
-//         message:"data recieved sucessfully",
-//         user:req.body
-//     });
-
-    
-// });
-
-// app.patch('/user',(req,res)=>{
-//     console.log('req.body->',req.body);
-//     //update data in user objxt
-//     let dataTobeUpdated=req.body;
-
-//     for(key in req.body)
-//     {
-//         users[key]=dataTobeUpdated[key];
-
-//     }
-//     res.json({
-//         message:"data updated sucessfully"
-//     })
-
-// })
-
-
-// app.delete('/user',(req,res)=>{
-//     users={};
-//     res.json({
-//         message:"data has been deleted"
-//     })
-// })
-// app.get('/user/:username',(req,res)=>{
-//     console.log(req.params.username);
-//     console.log(req.params);
-
-//     res.send("user id id recienced");
-   
-
-// })
-
-
-
-
-
-
-
+//mini app
+const userRouter = require('./Routers/userRouter');
+const planRouter = require('./Routers/planRouter');
+const reviewRouter = require('./Routers/reviewRouter');
+const bookingRouter=require('./Routers/bookingRouter');
+//base route , router to use
+app.use("/user", userRouter);
+app.use("/plans", planRouter);
+app.use("/review", reviewRouter);
+app.use('/booking',bookingRouter);
+// app.use("/auth", authRouter);
 
 
 
